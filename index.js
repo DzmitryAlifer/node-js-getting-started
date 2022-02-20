3bb4f9199ae79f159402734279cf8d16be14dc85
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-20 12:51:39 +0100

    1

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://safe-crag-81937.herokuapp.com/users');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
