bc8cfb03f11b127ee8f4dfdd07d3d6ea94970816
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-20 12:36:00 +0100

    1

--- index.js
+++ index.js
@@ -14,28 +14,18 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
-  .get('/users', async (req, res) => {
+  .get('/users', async (request, response) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM users');
       const results = { 'results': (result) ? result.rows : null};
       console.log(results);
-      res.render('pages/db', results );
+      response.render('pages/db', results);
       client.release();
     } catch (err) {
       console.error(err);
-      res.send('Error: ' + err);
+      response.send('Error: ' + err);
     }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
-
-  const getUsers = (request, response) => {
-    pool.query('SELECT * FROM users', (error, results) => {
-      if (error) {
-          throw error;
-      }
   
-      response.status(200).json(results.rows);
-    });
-  };
