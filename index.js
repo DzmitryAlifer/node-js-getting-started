d5586262f1c9f0b507074ad3aacec49becf083f8
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-02-26 09:55:31 +0100

    1

--- index.js
+++ index.js
@@ -10,40 +10,11 @@ const pool = new Pool({
   ssl: {rejectUnauthorized: false},
 });
 
-
 const GET_ALL_USERS_SQL = 'SELECT id, username, firstname, lastname FROM users;';
 const GET_USER_BY_ID_SQL = 'SELECT id, username, firstname, lastname FROM users WHERE id = $1;';
 const LOG_IN_SQL = 'SELECT id, username, firstname, lastname FROM users WHERE username = $1 AND password = $2;';
 const CREATE_USER_SQL = 'insert into public.users (username, password, firstname, lastname) values ($1, $2, $3, $4);';
 const GET_USER_BY_ID_SQL = 'SELECT id, username, firstname, lastname FROM users WHERE id = $1;';
-
-
-const getUserById = async (request, response) => {
-  const id = parseInt(request.params.id);
-  const client = await pool.connect();
-  const resultSet = await client.query(GET_USER_BY_ID_SQL, [id]);
-  response.json(resultSet.rows[0]);
-  client.release();
-};
-
-const createUser = async (request, response) => {
-  const params = [request.body.username, request.body.password, request.body.firstname, request.body.lastname];
-  const client = await pool.connect();
-  const resultSet = await client.query(CREATE_USER_SQL, params);
-  response.json(resultSet.rows[0]);
-  client.release();
-};
-
-const login = async (request, response) => {
-  const params = [request.body.username, request.body.password];
-  const client = await pool.connect();
-  const resultSet = await client.query(LOG_IN_SQL, params);
-  resultSet.rows.length === 1 ? 
-      response.status(200).json(resultSet.rows[0]) : 
-      response.status(401).json(null);
-  client.release();
-};
-
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -65,7 +36,27 @@ app.get('/users', async (request, response) => {
   response.json(resultSet.rows);
   client.release();
 });
-app.get('/users/:id', getUserById);
-app.post('/users', createUser);
-app.post('/login', login);
+app.get('/users/:id', async (request, response) => {
+  const id = parseInt(request.params.id);
+  const client = await pool.connect();
+  const resultSet = await client.query(GET_USER_BY_ID_SQL, [id]);
+  response.json(resultSet.rows[0]);
+  client.release();
+});
+app.post('/users', async (request, response) => {
+  const params = [request.body.username, request.body.password, request.body.firstname, request.body.lastname];
+  const client = await pool.connect();
+  const resultSet = await client.query(CREATE_USER_SQL, params);
+  response.json(resultSet.rows[0]);
+  client.release();
+});
+app.post('/login', async (request, response) => {
+  const params = [request.body.username, request.body.password];
+  const client = await pool.connect();
+  const resultSet = await client.query(LOG_IN_SQL, params);
+  resultSet.rows.length === 1 ? 
+      response.status(200).json(resultSet.rows[0]) : 
+      response.status(401).json(null);
+  client.release();
+});
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
