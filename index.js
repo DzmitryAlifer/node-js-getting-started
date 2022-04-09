e61445f9dc0f4a16192c7ccd7c94b60ec2376670
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-04-09 23:43:30 +0200

    1

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ const updateUsersPoints = async (request, response) => {
 
 const updateUserAvatar = async (request, response) => {
   const params = [request.body.userid, request.body.avatar];
-  console.log(request.body);
+  console.log(request);
   const client = await pool.connect();
   const resultSet = await client.query(UPDATE_USER_AVATAR_SQL, params);
   response.json(request.body);
