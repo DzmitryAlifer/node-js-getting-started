f0a3a9e2f97c96992897b19efb3c263e067050b7
Dzmitry Alifer <dzmitry.alifer@gmail.com>
2022-04-21 20:57:52 +0200

    1

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const pool = new Pool({
 });
 
 
+
 const GET_ALL_USERS_SQL = 'SELECT id, username, firstname, lastname, seasonpoints, season_events_total, avatar, admin FROM users ORDER BY seasonpoints DESC;';
 const GET_USER_BY_ID_SQL = 'SELECT id, username, firstname, lastname, seasonpoints, season_events_total, avatar, admin FROM users WHERE id = $1;';
 const LOG_IN_SQL = 'SELECT id, username, firstname, lastname, seasonpoints, season_events_total, avatar, admin FROM users WHERE username = $1 AND password = $2;';
